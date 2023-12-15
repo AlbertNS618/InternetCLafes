63cdbc0e8ccd0292dbd9801d0841ec091077c8f9
Sabrinannis <153888493+Sabrinannis@users.noreply.github.com>
2023-12-15 12:56:39 +0700

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -12,18 +12,16 @@ public class User {
 	private String UserName;
 	private String Password;
 	private Integer UserAge;
-	private String UserRole;
 	
 	
 	
-	public User(int count, String userName, String password, Integer userAge, String role) {
+	public User(int count, String userName, String password, Integer userAge) {
 		super();
 		counts  = new AtomicInteger(count);
 		UserID = counts.incrementAndGet();
 		UserName = userName;
 		Password = password;
 		UserAge = userAge;
-		UserRole = role;
 	}
 
 	public String getUserName() {
@@ -50,14 +48,6 @@ public class User {
 		UserAge = userAge;
 	}
 	
-	public String getUserRole() {
-		return UserRole;
-	}
-
-	public void setUserRole(String userRole) {
-		UserRole = userRole;
-	}
-	
 	public static boolean checkUsernameAndPassword(String name, String pass){
 		ConnectDB db = ConnectDB.getInstance();
 		boolean registered = false;
@@ -72,12 +62,11 @@ public class User {
 		// TODO Auto-generated method stub			
 		ConnectDB db = ConnectDB.getInstance();
 
-		db.executePrepUpdate("INSERT INTO userclafes (UserName, Password, UserAge, UserRole) VALUES (?, ?, ?, ?)", preparedStatement->{
+		db.executePrepUpdate("INSERT INTO userclafes (UserName, Password, UserAge) VALUES (?, ?, ?)", preparedStatement->{
 		try {
 			preparedStatement.setString(1, user.getUserName());
 			preparedStatement.setString(2, user.getPassword());
 			preparedStatement.setInt(3, user.getUserAge());
-			preparedStatement.setString(4, user.getUserRole());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
